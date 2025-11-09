61e0721dc02ed5e0ef03fb9164cb0a201d750245
satishsahu25 <satishk7619@gmail.com>
2025-11-09 19:28:10 +0900

    frontend

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://your-frontend.example'], 
+  origin: ['http://localhost:5173', 'https://theunfoldedpassport-v1.netlify.app/'], 
   credentials: true, // <-- required to allow cookies
 }));
 
